e2849dabfd0f9e5a8db0e263808e6ddd263e0bee
neutra <neutra@qq.com>
2015-03-11 03:18:01 +0800

    >> 发冒泡页面
    Conflicts:
    	app/src/main/java/net/coding/program/maopao/MaopaoAddActivity.java
    	app/src/main/res/values/strings.xml

---
+++ app/src/main/java/net/coding/program/model/LocationObject.java
@@ -0,0 +1,38 @@
+package net.coding.program.model;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+
+/**
+ * Created by Neutra on 2015/3/11.
+ */
+public class LocationObject implements Serializable {
+    public static enum Type {Undefined, City, Normal}
+
+    public final Type type;
+    public final String id, name, address;
+
+    public LocationObject(JSONObject json) throws JSONException {
+        type = Type.Normal;
+        name = json.getString("name");
+        address = json.has("address") ? json.getString("name") : "";
+        id = json.optString("id");
+    }
+
+    public LocationObject(Type type, String id, String name, String address) {
+        this.type = type;
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public static LocationObject undefined() {
+        return new LocationObject(Type.Undefined, null, "不显示位置", null);
+    }
+
+    public static LocationObject city(String city) {
+        return new LocationObject(Type.City, null, city, null);
+    }
+}
