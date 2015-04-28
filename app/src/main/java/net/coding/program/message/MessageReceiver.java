7ddf8e5b09f14e39f167d1356977ae8cf1b5f7a0
chenchao <8206503@qq.com>
2015-04-28 11:42:24 +0800

    监听网络连接

---
+++ app/src/main/java/net/coding/program/message/MessageReceiver.java
@@ -0,0 +1,30 @@
+package net.coding.program.message;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+/**
+ * Created by chenchao on 15/4/28.
+ */
+public class MessageReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("", "my network " + isOnline(context));
+        if (isOnline(context)) {
+
+        }
+    }
+
+    private boolean isOnline(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return (netInfo != null && netInfo.isConnected());
+
+    }
+
+}
