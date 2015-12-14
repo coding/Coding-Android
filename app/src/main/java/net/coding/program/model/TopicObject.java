eb7e8955b2fea1ba0f61d298d17a6a215c5c09ce
chenchao <8206503@qq.com>
2015-12-14 14:58:21 +0800

    解决了一些合并冲突

--- app/src/main/java/net/coding/program/model/TopicObject.java
+++ app/src/main/java/net/coding/program/model/TopicObject.java
@@ -13,7 +13,6 @@ import org.json.JSONObject;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 /**
  * Created by cc191954 on 14-8-18.
@@ -31,7 +30,6 @@ public class TopicObject extends BaseComment implements Serializable {
     public long updated_at;
     public ArrayList<TopicLabelObject> labels = new ArrayList<>();
     public int comment_count;
-    public List<TopicLabelObject> labels = new ArrayList<>();
     private int number;
     private int commentSort = SORT_OLD;
     public TopicObject(JSONObject json) throws JSONException {
