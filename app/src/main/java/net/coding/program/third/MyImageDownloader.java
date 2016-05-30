3c9103d6b5c3d3db177292c3f06354cb43a758ed
8206503 <8206503@qq.com>
2016-05-30 13:33:50 +0800

    删除了一些不用的代码

--- app/src/main/java/net/coding/program/third/MyImageDownloader.java
+++ app/src/main/java/net/coding/program/third/MyImageDownloader.java
@@ -11,15 +11,7 @@ import org.apache.http.cookie.Cookie;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
 import java.util.List;
-
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 
 /**
  * Created by chaochen on 14-10-7.
@@ -28,30 +20,11 @@ public class MyImageDownloader extends BaseImageDownloader {
 
     public MyImageDownloader(Context context) {
         super(context);
-        initSSLSocketFactory();
-    }
-
-    private SSLSocketFactory sf;
-
-    private void initSSLSocketFactory() {
-        try {
-            SSLContext sslContext = SSLContext.getInstance("TLS");
-
-            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-            sf = sslContext.getSocketFactory();
-
-        } catch (Exception e) {
-            Global.errorLog(e);
-        }
     }
 
     @Override
     protected HttpURLConnection createConnection(String url, Object extra) throws IOException {
         HttpURLConnection conn = super.createConnection(url, extra);
-
-        if (conn instanceof HttpsURLConnection) {
-            ((HttpsURLConnection) conn).setSSLSocketFactory(sf);
-        }
 
         if (url.startsWith(Global.HOST)) {
             PersistentCookieStore cookieStore = new PersistentCookieStore(context);
@@ -71,19 +44,4 @@ public class MyImageDownloader extends BaseImageDownloader {
         return conn;
     }
 
-    TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
-        public X509Certificate[] getAcceptedIssuers() {
-            return null;
-        }
-
-        @Override
-        public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
-            // Not implemented
-        }
-
-        @Override
-        public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
-            // Not implemented
-        }
-    }};
 }
