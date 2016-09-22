d0d92147aad2846a2313e61a896884d6a55ba5d4
8206503 <8206503@qq.com>
2016-09-22 10:39:12 +0800

    帮助与反馈 UI 修改

--- app/src/main/java/net/coding/program/setting/HelpActivity.java
+++ app/src/main/java/net/coding/program/setting/HelpActivity.java
@@ -1,28 +1,24 @@
 package net.coding.program.setting;
 
 import android.view.Menu;
-import android.view.MenuInflater;
 
 import net.coding.program.R;
 import net.coding.program.WebActivity;
 
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.OptionsItem;
 
 
-@EActivity(R.layout.activity_web)
+@EActivity(R.layout.activity_help)
 public class HelpActivity extends WebActivity {
 
-    @OptionsItem
-    void actionFeedback() {
-        actionbarTitle.setText("帮助与反馈");
+    @Click
+    void toolbarFeedback() {
         FeedbackActivity_.intent(this).start();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.help, menu);
         return true;
     }
 }
