a1db0ff7baa4b54cadc3378e44c4b0b1faf4da40
8206503 <8206503@qq.com>
2017-04-12 12:04:20 +0800

    treeview 控件基本完成

--- app/src/main/java/net/coding/program/project/detail/wiki/NodeHolder.java
+++ app/src/main/java/net/coding/program/project/detail/wiki/NodeHolder.java
@@ -3,13 +3,17 @@ package net.coding.program.project.detail.wiki;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.unnamed.b.atv.model.TreeNode;
 
 import net.coding.program.R;
+import net.coding.program.common.Global;
 import net.coding.program.network.model.wiki.Wiki;
+
+import org.greenrobot.eventbus.EventBus;
 
 /**
  * Created by chenchao on 2017/4/11.
@@ -18,6 +22,7 @@ public class NodeHolder extends TreeNode.BaseNodeViewHolder<Wiki> {
 
     private ImageView icon;
     private TextView title;
+    private View iconLayout;
 
     public NodeHolder(Context context) {
         super(context);
@@ -26,15 +31,42 @@ public class NodeHolder extends TreeNode.BaseNodeViewHolder<Wiki> {
     @Override
     public View createNodeView(TreeNode node, Wiki value) {
         View v = LayoutInflater.from(context).inflate(R.layout.wiki_tree_item, null, false);
+        iconLayout = v.findViewById(R.id.iconLayout);
         icon = (ImageView) v.findViewById(R.id.icon);
         title = (TextView) v.findViewById(R.id.title);
         title.setText(value.title);
+        View bottomLine = v.findViewById(R.id.bottomLine);
 
+        int iconWidth; // icon 到文字的距离，包括 icon 本身的宽度
         if (value.children == null || value.children.isEmpty()) {
             icon.setVisibility(View.GONE);
+            iconWidth = 0;
         } else {
             icon.setVisibility(View.VISIBLE);
+            iconWidth = 22;
         }
+
+        int level = node.getLevel();
+
+        int leftSpace = (level - 1) * 30 + 20 + iconWidth;
+        int leftSpacePx = Global.dpToPx(leftSpace);
+
+        ViewGroup.MarginLayoutParams iconLP = (ViewGroup.MarginLayoutParams) iconLayout.getLayoutParams();
+        iconLP.width = leftSpacePx;
+        iconLayout.setLayoutParams(iconLP);
+
+        ViewGroup.MarginLayoutParams lineLP = (ViewGroup.MarginLayoutParams) bottomLine.getLayoutParams();
+        lineLP.leftMargin = leftSpacePx - Global.dpToPx(iconWidth);
+        bottomLine.setLayoutParams(lineLP);
+
+        title.setTag(value);
+        title.setOnClickListener(v1 -> {
+            if (value.children != null && !value.children.isEmpty()) {
+                node.setExpanded(true);
+            }
+
+            EventBus.getDefault().post(NodeHolder.this);
+        });
 
         return v;
     }
@@ -46,5 +78,14 @@ public class NodeHolder extends TreeNode.BaseNodeViewHolder<Wiki> {
 
     @Override
     public void toggleSelectionMode(boolean editModeEnabled) {
+    }
+
+    public void select(boolean select) {
+        if (select) {
+            tView.expandNode(mNode);
+            title.setTextColor(0xFF2EBE76);
+        } else {
+            title.setTextColor(mNode.getLevel() == 1 ? 0xFF272C33 : 0xFF323A45);
+        }
     }
 }
--- app/src/main/java/net/coding/program/project/detail/wiki/WikiMainActivity.java
+++ app/src/main/java/net/coding/program/project/detail/wiki/WikiMainActivity.java
@@ -18,6 +18,9 @@ import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.Extra;
 import org.androidannotations.annotations.ViewById;
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,6 +38,10 @@ public class WikiMainActivity extends BackActivity {
     ViewGroup drawerLayout;
 
     List<Wiki> dataList = new ArrayList<>();
+    AndroidTreeView treeView;
+
+    NodeHolder selectNode = null;
+    TreeNode firstTreeNode = null;
 
     @AfterViews
     void initWikiMainActivity() {
@@ -65,23 +72,58 @@ public class WikiMainActivity extends BackActivity {
                 });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onEvent(NodeHolder node) {
+        selectNode(node);
+    }
+
     private void buildTree() {
         TreeNode root = TreeNode.root();
         addTreeNode(root, dataList);
 
-        AndroidTreeView treeView = new AndroidTreeView(this, root);
+        treeView = new AndroidTreeView(this, root);
         treeView.setDefaultViewHolder(NodeHolder.class);
-        treeView.setDefaultNodeClickListener((node, value) -> {
-            Wiki wiki = (Wiki) value;
-        });
         drawerLayout.addView(treeView.getView());
 
+        selectNode((NodeHolder) firstTreeNode.getViewHolder());
+    }
+
+    private void selectNode(NodeHolder node) {
+        if (selectNode == node) {
+            return;
+        }
+
+        if (selectNode != null) {
+            selectNode.select(false);
+        }
+
+        selectNode = node;
+
+        if (selectNode != null) {
+            selectNode.select(true);
+        }
     }
 
     private void addTreeNode(TreeNode node, List<Wiki> wikis) {
         for (Wiki item : wikis) {
             TreeNode childNode = new TreeNode(item);
             node.addChild(childNode);
+            if (firstTreeNode == null) {
+                firstTreeNode = childNode;
+            }
+
             addTreeNode(childNode, item.children);
         }
     }
