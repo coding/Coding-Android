29ec14165029db9f97322d391b1cb703491ffb58
8206503 <8206503@qq.com>
2017-11-29 16:50:38 +0800

    添加微信支付

---
+++ pay-coding/src/main/java/net/coding/program/pay/PayBroadcast.java
@@ -0,0 +1,12 @@
+package net.coding.program.pay;
+
+/**
+ * Created by chenchao on 2017/11/29.
+ */
+
+public class PayBroadcast {
+
+    public static final String WEIXIN_PAY_INTENT = "NET.CODING.PROGRAM.PAYACTIVTY.WEIXIN_PAY_RESULT";
+    public static final String WEIXIN_PAY_PARAM = "NET.CODING.PROGRAM.PAYACTIVTY.WEIXIN_PAY_PARAM";
+
+}
---
+++ pay-coding/src/main/java/net/coding/program/pay/PayKeys.java
@@ -0,0 +1,11 @@
+package net.coding.program.pay;
+
+/**
+ * Created by chenchao on 2017/11/29.
+ */
+
+public class PayKeys {
+
+    public static final String WX_APP_ID = "wx6ef6df9e9706b1bd";
+
+}
