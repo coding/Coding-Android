90738dfc201224d5fbab9c45e6808451098ebd6c
8206503 <8206503@qq.com>
2017-11-30 09:29:43 +0800

    添加PaymentActivity

---
+++ app/src/main/java/net/coding/program/mall/PaymentActivity.java
@@ -0,0 +1,24 @@
+package net.coding.program.mall;
+
+import android.app.LauncherActivity;
+
+import net.coding.program.R;
+import net.coding.program.common.ui.BackActivity;
+import net.coding.program.common.widget.ListItem1;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * Created by chenchao on 2017/11/29.
+ */
+
+@EActivity
+public class PaymentActivity extends BackActivity {
+
+    @AfterViews
+    void initPaymentActivity() {
+    }
+
+
+}
