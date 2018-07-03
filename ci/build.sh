7a40ad089382dbf6e0993e401a15be15e95de5e9
chenchao <cc191954@gmail.com>
2018-07-03 16:38:53 +0800

    docker 编译基本完成

--- ci/build.sh
+++ ci/build.sh
@@ -4,5 +4,5 @@ docker_src=/home/data
 
 docker rm ci2
 docker run --mount type=bind,source=$code_dir,target=$docker_src -it --name ci2 android-ci:v2 \
-	bash -c "cd ${docker_src} && export PATH=$PATH:/opt/gradle/gradle-4.4/bin && gradle wrapper && ./gradlew assembleRelease"
+	bash -c "cd ${docker_src} && export PATH=$PATH:/opt/gradle/gradle-4.4/bin && gradle wrapper && ./gradlew assembleEnterprise"
 
